c05cd3b131e27dd096b17c8172d3204bd7e8d170
emirhan <116457536+emirhanerdem@users.noreply.github.com>
2025-03-05 13:32:58 +0300

    Add files via upload

---
+++ code/lecture5_modules.py
@@ -0,0 +1,29 @@
+#python built-in modules -> math , date (python yüklendiğinde hazır olarak gelen kütüphaneler)
+#local modules -> lecture2_functions.py , lecture4_inheritance.py , lecture3_oop.py (kendi oluşturduğumuz modüller)
+#3rd party libraries -> (pip ile indirebildiğimiz paketler) request vs..
+
+#import math #math kütüphanesini bu dosaya dail et
+
+# print(math.sqrt(25))
+# print(math.pi)
+
+#---------------------------------------
+
+# from math import sqrt,pi
+
+# print(sqrt(25))
+# print(pi)
+
+#---------------------------------------
+#alias -> takma ad
+# from math import sqrt as karekok, pi as pi_sayisi
+
+# print(karekok(25))
+# print(pi_sayisi)
+
+
+#---------------------------------------
+# from lecture4_inheritance import Car #local modules
+
+# car = Car("Hyundai")
+# car.start()
